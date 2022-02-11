927b90c4d47c1673a83dbca193859ee941d9cb87
Veljko Potparic <veljko.potparic@united.cloud>
2022-02-11 02:02:54 +0100

    added remote url in pipeline

--- createJobs.groovy
+++ createJobs.groovy
@@ -1,8 +1,14 @@
-pipelineJob('pipelineJob') {
+pipelineJob('theme-park-job') {
     definition {
-        cps {
-            script(readFileFromWorkspace('pipelineJob.groovy'))
-            sandbox()
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url 'https://github.com/velizarvel/spring-boot-api-for-docker-jenkins-awt.git'
+                    }
+                    branch 'master'
+                }
+            }
         }
     }
 }
